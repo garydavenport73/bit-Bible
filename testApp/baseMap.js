4ac302829d5fa00b41275181492014f1b18dddc7
garydavenport73 <software@garydavenport.com>
2022-07-31 20:10:22 -0400

    got map working mobile

---
+++ testApp/baseMap.js
@@ -0,0 +1,133 @@
+let baseMap = "ZaqOYXYUUUYYXXXXXXXXXUZYzzzzzzzzccccvvvvvvuvuuuuuuuuunnnxxrxxxnnCcnxrrrrrxrrfffttttttfftYtttttxzfttttttttzztt////////////////tLh\n\
+Zz>IIIIIIIIIIIIIIIIIIi1iIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII;ciIIIIIIIIIIIIIIIIIIIIIrI               .[[.                 .cb\n\
+ZziIIIIIIIIIIIIIIIIII!1iIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII;nIIIIIIIIIIIIIIIIIIIII|<                .[{.                 .vb\n\
+ZciIIIIIIIIIIIIIIIIII!{iIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIn!IIIIIIIIIIIIIIIIIII--.            ....i])^                 .rd\n\
+0/lIIIIIIIIIIIIIIIIII>t>IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIik;IIIIIIIIIIIIIIIIII;[!In];,^^^^^^,;~u){+~-.    I.           ^zb\n\
+0tlIIIIIIIIIIIIIIIIIIit>IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII!z;IIIIIIIIIIIIIIIIIIi1^  .I<~<~]]-!..I{I.Il    ;|:           .jd\n\
+0flIIIIIIIIIIIIIIIIIIi|iIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII!v;IIIIIIIIIIIIIIIIII1;             .[-^  l!   lr,             fd\n\
+0/lIIIIIIIIIIIIIIIIII!|iIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIizIIIIIIIIIIIIIIIIII(>            .~/^ .)+l!  ln;              fd\n\
+0/IIIIIIIIIIIIIIIIIII!/>IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIiu;IIIIIIIIIIIIIII;)~            i};  ^(l I! .+<               |p\n\
+0tIIIIIIIIIIIIIIIIIII!|iIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII!c;IIIIIIIIIIIIIII_-.          ^);   .-<  Il  <~.              tp\n\
+0flIIIIIIIIIIIIIIIIII!|iIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIlu:IIIIIIIIIIIIII>|.          .-I   .<(.  ;!    +}I.           tp\n\
+Ox!IIIIIIIIIIIIIIIIII!|iIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIx:IIIIIIIIIIIIIlx,          ^{_   ;)~.   Ii.     :+]I         |p\n\
+ZL+IIIIIIIIIIIIIIIIIIi|iIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIn;IIIIIIIIIIII:|;           <}   !{:     Ii         _?^.   ...|d\n\
+OrIIIIIIIIIIIIIIIIIIIi|iIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIlu:IIIIIIIIIIIl[<           ;f.  it:      Ii.          <vzxXXv[tp\n\
+0t;IIIIIIIIIIIIIIIIIIi/iIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIlv;IIIIIIIIII?};           ,t, .In:       Ii.                  }q\n\
+0/;IIIIIIIIIIIIIIIIII>t>IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIv;IIIIIIIIIi|           ,)_^.>t-.        I~.                 .{q\n\
+0/;IIIIIIIIIIIIIIIIII>t>IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIlu;IIIIIIII;/|_-!.     .It: ,(_           l+.      .:+}{-~[(1}}vh\n\
+0|;IIIIIIIIIIIIIIIIIIi/iIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIln;IIIIIII;~_  ;}1])|f(|-. ,fI            !_. !}?+_[_^         {q\n\
+0|;IIIIIIIIIIIIIIIIIIi|iIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIlv;IIIIII;-<              .)I             !_.                  {q\n\
+0(;IIIIIIIIIIIIIIIIII>/iIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIuIIIIIIIl+               ,(;             l_.                  {q\n\
+0(;IIIIIIIIIIIIIIIIII</iIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIn;IIIIII>+               ^/l             l_^                  {q\n\
+0);IIIIIIIIIIIIIIIIII<|iIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIr;IIIIII1}.              ~[.             I_^                  {q\n\
+0);IIIIIIIIIIIIIIIIII<(iIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIx;IIIIi/I:_<,            I1:             I_^                  1q\n\
+0);IIIIIIIIIIIIIIIIII~(iIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIx;III-<.   ^<+^          ./<             I?^                  }q\n\
+0);IIIIIIIIIIIIIIIIII~(!IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIr;II<-^.....             I{r^            ;?^                  [q\n\
+0);IIIIIIIIIIIIIIIIII~)!IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIlrIII(t1]~l+_..    ... ...l[u;............I?,..  . ...  ..  . .[q\n\
+qv{1[?[}[[}}[]?]??][}tz{???]]]?--_-???????]]]]]]][[[[[[[[[}{)((t0||/wf/t//frxxxrjjjjtjrxrxQCxrjjfjrxxxxnncLvnnuvuXYrttttjfrrj/Xa\n\
+Q)IIIIIIIIIIIIIIIIIII+1IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII;;t^^>]^..                 ,]:             :-,    .__           ?w\n\
+Q1IIIIIIIIIIIIIIIIIII~{;IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII!t^,/<,^..,,             .:]:             ;?,    !1^           -w\n\
+Q1IIIIIIIIIIIIIIIIIII~};IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIl/,!fl!!<I,.           .l)t/_.            ;?,  ._t.            -w\n\
+Q1IIIIIIIIIIIIIIIIIII~[;IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIl+r_/;..               .-runuf^.           :?,.:f>              -w\n\
+Q1IIIIIIIIIIIIIIIIIII+};IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIlrXr+I^..               .<xuuux^           .;];~jl               _w\n\
+Q{IIIIIIIIIIIIIIIIIII+};IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII+[>1,>i..                .izuu)^...^,,^...^:)mQ].                _w\n\
+Q{IIIIIIIIIIIIIIIIIII+};IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIl}?i{^^{I...              ..!jn[^;-Ux)XqzzvUw({:.                 _w\n\
+Q{IIIIIIIIIIIIIIIIIII+];IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII;?>i[^.,}l..              ..:_;?zf>..     ..:tZv?,.               +w\n\
+Q}IIIIIIIIIIIIIIIIIII_[;IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII);>]^..^--,...  .        ..!Ovi^            :?I<f;               +w\n\
+Q}IIIIIIIIIIIIIIIIIII-[;IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIl},>?^......:~i.           .l+.              :_,                  ~w\n\
+Q}IIIIIIIIIIIIIIIIIII-[;IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII((,<]^....   .!>.          .:-.              :-,                  ~w\n\
+Q[IIIIIIIIIIIIIIIIIII-[:IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIf-^<]^...      !:.        ..{l               :?,                  ~w\n\
+Q[IIIIIIIIIIIIIIIIIII?]:IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII+Y,^~?....     .I:          .{:               :?,                  ~w\n\
+Q]IIIIIIIIIIIIIIIIIII??:IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIu?^^<?....     .i,          .+>.              :-,                  ~w\n\
+Q]IIIIIIIIIIIIIIIIIII??:IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII[r^^^<?...     ^I^          .;].               :?,                  <w\n\
+Q]IIIIIIIIIIIIIIIIIII-_:IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII<v!^^^<?...     ..          ..<>.               :?,                  <m\n\
+Q]IIIIIIIIIIIIIIIIIII?_,IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII{|,...<-..                 . .,{.               :?,                  <m\n\
+Q]IIIIIIIIIIIIIIIIIII+<,IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIt-....<-....           . .....^1.               :?,                  >m\n\
+L]IIIIIIIIIIIIIIIIIII-+,IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIivl....<_..              ....^.^{:              .:-,                  >m\n\
+L]IIIIIIIIIIIIIIIIIII?+,IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIn|^....<-.                 ....^}.  .....^^:i-}_:;?,                  >m\n\
+L]IIIIIIIIIIIIIIIIIII?_,IIIIIIIIIIIIIIIIIIIIIIIIIIIIIII_0^.....<?.                  .^^:)^,{j]+)L0[^.   l?],                  >m\n\
+Q]IIIIIIIIIIIIIIIIIII?+,IIIIIIIIIIIIIIIIIIIIIIIIIIIIII:C_^^,I>!+1l!^.             ..^^^{vffI     ..      ,-,                  >m\n\
+Q]IIIIIIIIIIIIIIIIIII]_,IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIjx,;(0+!<rX+<I.             ....^{..               ,_^                  <m\n\
+L]IIIIIIIIIIIIIIIIIII?_,IIIIIIIIIIIIIIIIIIIIIIIIIIIII>(I>>,.  .i].                  ..+_.                ,-,.                 >m\n\
+Q]IIIIIIIIIIIIIIIIIII]_,IIIIIIIIIIIIIIIIIIIIIIIIIII;lr~........i[..             .   .,{;.               .,-,.                 >w\n\
+Q]IllllllllllIlll;;ll}[;;;;;;IIIIIIIIIll!!!!!ii>>>>>tu<<<<<<<~~1j+_-?????]]]]]]]][[}1(Uj///tfrnunuuuuvvvvzQYUJJUJCCJJUXvj/cXJJZM\n\
+Q?IIIIIIIIIIIIIIIIII,+_II;^^IIIIIIIII^.,III:;;:III;x/..       .i].                  ..^[.       .. ...,i.,-:..                >m\n\
+L?IIIIIIIIIIIIIIIIII,_]IIIIIIIIIIIIIIIIIIIIIIIIIII[vui.  ..   .>[.    ^...          ..I[.         ..it{, ^_:....              >m\n\
+L?IIIIIIIIIIIIIIIIII,_]IIIIIIIIIIIIIIIIIIIIIIIIII}v;,|].      .<}... .,:::.         .,?~.    .^,I[v]l.   ^-:.....             >m\n\
+L?IIIIIIIIIIIIIIIIII,>+IIIIIIIIIIIIIIIIIIIIIIIIIIn! .,ti.   . .+[....^;;..           .>?<_-}UC_.         ^-;,,^^^.            im\n\
+L?IIIIIIIIIIIIIIIIII,_]IIIIIIIIIIIIIIIIIIIIIIII,n+.   IX):....._]...^,;;,^.          ^ir-.               ^-:^,:::.            >m\n\
+L?IIIIIIIIIIIIIIIIII,-]IIIIIIIIIIIIIIIIIIIIIIIIu{       ^]O/,.._[. .IIl:.          .!nvu1.               ._, ..^,.            im\n\
+L?IIIIIIIIIIIIIIIIII,+-IIIIIIIIIIIIIIIIIIIIIII|)            ..._?.   ..           ^}uuuun?               ._,                  im\n\
+L?IIIIIIIIIIIIIIIIII,<+IIIIIIIIIIIIIIIIIIIIIl)f^              ._?. .             .+tnnuux-               .+^                  im\n\
+L?IIIIIIIIIIIIIIIIII,+-IIIIIIIIIIIIIIIIIIII^r(.               ._-.               >{xtuuuzl               .+^                  >m\n\
+L?IIIIIIIIIIIIIIIIII,~_IIIIIIIIIIIIIIIIII;lU]                 ._?.              ,|uuuuuu)                .+,                  im\n\
+Q?IIIIIIIIIIIIIIIIII,<~IIIIIIIIIIIIIIIIII?n_}?,.........     ..-].              ?uruuunu?                .+^                  im\n\
+Q?IIIIIIIIIIIIIIIIII,<~IIIIIIIIIIIIIIIIIx-.  .;(n00zXOQYv:.....?[.              }tuuruuc;                ._,                  >m\n\
+Q?IIIIIIIIIIIIIIIIII,IIIIIIIIIIIIIIIII>/,               .>n!...-[.              _uuunrur{.               ._,                  iw\n\
+Q?IIIIIIIIIIIIIIIIII:<<IIIIIIIIIIIIII-]^.                     .-].             .{uunxunuc>iI,^.^^^^.     .-,                  iw\n\
+Q?IIIIIIIIIIIIIIIIII:~~IIIIIIIIIIIIlzl{/:..                  .._].             .tuuuuuuu?    .... ^nQl.  .]:                  >w\n\
+Q?IIIIIIIIIIIIIIIIII:+~IIIIIIIIII>{i...^/X{_^.               ..+[.             lznnuuuun^           i0-. .?,                  >w\n\
+Q?IIIIIIIIIIIIIIIIII:_+IIIIIIII~f>...   .][I]?.              ..+].             ,unuu/xv{             [n: .?,                  iw\n\
+Q?IIIIIIIIIIIIIIIIII:_+IIIIIl[),.       .~|,                 ..+].             .(juvLz|,             ;J/:^],                  >w\n\
+Q?IIIIIIIIIIIIIIIIII:_<II![|+..         .?j:                  ._].             Inuc},!,                ,{vJ!.                 iw\n\
+Q?IIIIIIIIIIIIIIIIl!:-<iX]:.            .i):.                 ._].            ./uuuv;.                   .1x0{^               iq\n\
+0[IIIIIIIIIIIIIII;;l+X-.                 .>n!,.... .          ._].            ,XuuuuuU{.                 .?, ..               iq\n\
+O]IIIIIIIIIIIi]]}1<^^-i                    .;}|1!...          .+].             /uuuxuX]                   -,                  iq\n\
+hqXxn1[]|t{~>1_.    ^]i                       ..-[^.          .~?.             ^Uuuuun_                  .-,                  >p\n\
+d_           ](^    ^[i                         .<-. .        ._[.              iUnuct^                   _,                  >p\n\
+O_          .,((^.. ,}i                           ...... ......_[...         .. .,~d(^..     .           .-,                  >p\n\
+Z-^^^^^^^^^^^^,u1,,,;1<,,,,:::;;;:::;;;;;;;;;;;;;I;;IIIllllllIl{(llllIllllllIllIllliLbOOLn+!iii<<ii!i!iiii/+>iiiii!!!!iiiiii>i[h\n\
+O_.         ...}1ni^:}i... .                      ............._].                       .,i~xJt{{~;..   .-:                  ik\n\
+Z_           .<?..!(nui..                                     .+].                               .i|c{:  .?,                  ik\n\
+Z_          .>~.  ..:)}C<^.                                   .+].                                  .;:  ._,                  ik\n\
+Z_        ..l?.     ,[l.:|[^.     .                           .+[.                                        _,                  ik\n\
+m_        .+t,      ,]l...(j^.                           ..  ..+[.                                        -,                  ih\n\
+m_       ._<Y.      :)! ..;Ol.                                .+].                                        -,                  ih\n\
+w_      ~(IL;       :{l ...u(.                                .+].                                        -,                  ia\n\
+w_      , ~[.       :1l   .!c:.                            ....+?.                                        _,                  ih\n\
+w_                  :{I   .lJl^...   .                        .-?.                                        _,                  ih\n\
+w_                  :1I  ..lJl!<]{-<i,..                     ..?-.                                        _,                  ik\n\
+q_                  :1;   ^}<1;....:;!__^                     .?-.                                        _,                  >k\n\
+q_                  :);. .<!..iX[^.                           .?-.                                        _,                  ik\n\
+q_                  ;|I^i/[^    ^}]^                          .]-.                                        _,                  ik\n\
+q_                 .;t1C},       ItI.                         .[-.                                        _,                  ik\n\
+q_               ..;cw-^.        .~ul.                        .[_.                                        _,                  ik\n\
+q_              .In)iLc[^.        .^u<.                      ..[~.                                        _,                  ik\n\
+q_              ;);.Iu/!),.        .t/.                      ..}<.                                        _,                  ik\n\
+q_              i<..It_+^<-:.   .   ;n+..                    ..1<.                                        _,                  ib\n\
+q_              ~<. I/>+...l],.      .+{<.                  ...)<.                                        _,                  >b\n\
+p_              !]..;|)-.  .!].         ,_?,                  .{<.                                        +,                  ik\n\
+q+              .l: InjI    :},            <+^                .{>                                         +,                  ib\n\
+q_               :: ;|l<                    ,-;               .1<.                                        +,                  ik\n\
+p_                  ;)^         ,[!          .l.              .)<.                                        +,                  ib\n\
+p_                  ;)^         ;ri                           .)<                                         +,                  ib\n\
+p_                  ;(^         ;f<                         ...(<.                                        +,                  ib\n\
+p-..................I/,^........;|!^...............^^.^.^^.^^^^|~^^^.^.......................^^^^^^^...^^^-;..................<b\n\
+MOJLCJJCJYXXXXXXXXXXUmx||)){1)(jxQQvunnxxxxrrjjfffffft//|||||/tLct/|||((/)1{}}[}[[[---_+_+_???---_+~~~<<<>)~!!i!!lIIlIII;;::::+k\n\
+w_                  I/^          ;(I               .^.        .(>.                                        ~,                  id\n\
+w+                  I/.           !r.                         .|>.                                        ~,                  id\n\
+m_                  I/.            ;(,                       ..|>.                                        +,                  id\n\
+m_                  I|.             ][.                       ./i.                                        +,                  id\n\
+Z_                  I|.             ;tI                       .|i.                                        +,                  id\n\
+Z+                  I(.             .{}.                      .|!                                         ~,                  id\n\
+O_                  I).              .!tI                     ./>.                                        ~,                  id\n\
+O+                  ;{.                                       ./>.                                        ~,                  ip\n\
+0+                  ;}.                                       .t>.                                        ~,                  ip\n\
+0_                  I).                                       .t>                                         <,                  ip\n\
+Q+                  l|                                      .<)X~.                                        <,                  ip\n\
+Q+                  l(                                    .;]uuO+.                                        <,                  ip\n\
+Q+                  l|                                  ,[xvuuxd<.                                        +,                  iq\n\
+Q+                  l)                                 :+nuuur(c>                                         _,                  iq\n\
+Q_                  I1                                .()uuuuf(Yi                                         +,                  iw\n\
+L+                  I)                               ,}[uuuuur/z!                                         -:                  iw\n\
+L+                  I{                              :/nuuuuuuc[rl                                         +,                  iw\n\
+L+                  I{                              ;rxuuuuuux~xl           .,                            <,                  iw\n\
+L+                  I{                             .Icuuuuutv|,rl                                         <,                  iw\n\
+L+                  ;+                             ._tuuuuuuxI.x1                                         -:                  iq\n\
+0+                  i(                             ;v{uuuuuv? .jr                                         f~                  ih\n\
+Q+                  ;{                              [uuuuuu<   _[                                         _;                  io\n\
+L+                  ;1.                           .juuuuuu].   ~?                                         +!                  i*\n\
+L+                  ;(.                          ,)uuuuuut;    <_                                         ]~                  ia\n\
+L+                  ,_.                         ^)uuuuuux~    .-<.                                        +<                  ib\n\
+wCccvvuuuvuuuvXzuuuuvUunnuuunnnnnnnnnnnnnnnnnnnnnnnnnxxxxxxxxxxcXxxxxxxxxxxxxxxxxxxxnxxnnnnnnnnnnnnuuuuvvvQOzzzzXXYUYYYYUXXbZCmp\n\
+";
