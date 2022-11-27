c272e3725cf665e0ab4070ea9925511371dae4d2
garydavenport73 <software@garydavenport.com>
2022-11-27 15:27:59 -0500

    making navigation single or all

---
+++ minimal/something.js
@@ -0,0 +1,58 @@
+if (evt.target.id === "map-Abraham") {
+    plotBackgroundLocation("Canaan","31.6935295036883","34.8438828857684","background-location");
+    plotBackgroundLocation("Ur","30.9625","46.103056","background-location");
+    plotBackgroundLocation("Haran","36.86","39.03139","background-location");
+    plotBackgroundLocation("Damascus","33.511612","36.309102","background-location");
+    plotBackgroundLocation("Shechem","32.22111","35.25444","background-location");
+    plotBackgroundLocation("Bethel","31.93053921","35.22103275","background-location");
+    plotBackgroundLocation("Dan","33.24856","35.65252","background-location");
+    // plotBackgroundLocation("Hobah","33.5","36.466667","background-location");
+    plotBackgroundLocation("Salem","31.777444","35.234935","background-location");
+    plotBackgroundLocation("Gerar","31.38176","34.60698","background-location");
+    plotBackgroundLocation("Beersheba","31.245","34.84068","background-location");
+    plotBackgroundLocation("Egypt","31.032047","33.854957","background-location");
+            } else if (evt.target.id === "map-Moses") {
+    plotBackgroundLocation("Sinai","29.5","34","background-location");
+    plotBackgroundLocation("Zin","31.6935295036883","34.8438828857684","background-location");
+    plotBackgroundLocation("Elim","29.306194","32.980924","background-location");
+    plotBackgroundLocation("Beer","30.998062","35.498547","background-location");
+    plotBackgroundLocation("Marah","29.35","32.933333","background-location");
+    plotBackgroundLocation("Red Sea","19","39.5","background-location");
+    plotBackgroundLocation("Wilderness of Sin","28.838778","33.420573","background-location");
+            } else if (evt.target.id === "map-12 Tribes") {
+    plotBackgroundLocation("Kedesh","32.559061","35.246206","background-location");
+    plotBackgroundLocation("Golan","32.80007553","35.9373013","background-location");
+    plotBackgroundLocation("Ramoth","32.049953","35.733402","background-location");
+    plotBackgroundLocation("Bezer","32.51613741","36.48829076","background-location");
+    plotBackgroundLocation("Hebron","31.524354","35.108539","background-location");
+    plotBackgroundLocation("Shechem","32.22111","35.25444","background-location");
+    plotBackgroundLocation("Damascus","33.511612","36.309102","background-location");
+    plotBackgroundLocation("Ammon","31.95522","35.94503","background-location");
+    plotBackgroundLocation("Jerusalem","31.777444","35.234935","background-location");
+    plotBackgroundLocation("Samaria","32.280231","35.197929","background-location");
+            } else if (evt.target.id === "map-Divided Kingdoms") {
+    plotBackgroundLocation("Shechem","32.22111","35.25444","background-location");
+    plotBackgroundLocation("Samaria","32.280231","35.197929","background-location");
+    plotBackgroundLocation("Jerusalem","31.777444","35.234935","background-location");
+    plotBackgroundLocation("Hebron","31.524354","35.108539","background-location");
+    plotBackgroundLocation("Gaza","31.503959","34.46203","background-location");
+    plotBackgroundLocation("Gerar","31.38176","34.60698","background-location");
+    plotBackgroundLocation("Damascus","33.511612","36.309102","background-location");
+    plotBackgroundLocation("Jezreel","32.502074","35.50196","background-location");
+            } else if (evt.target.id === "map-Jesus") {
+    plotBackgroundLocation("Bethlehem","31.70431","35.20746","background-location");
+    plotBackgroundLocation("Damascus","33.511612","36.309102","background-location");
+    plotBackgroundLocation("Phillipi","41.01316","24.28409","background-location");
+    plotBackgroundLocation("Nazareth","32.701029","35.300148","background-location");
+    plotBackgroundLocation("Jezreel","32.502074","35.50196","background-location");
+    plotBackgroundLocation("Samaria","32.280231","35.197929","background-location");
+    plotBackgroundLocation("Sea Galilee","32.8138046","35.590564","background-location");
+    plotBackgroundLocation("Dead Sea","31.577850349495","35.52394311","background-location");
+    plotBackgroundLocation("Mt. Ebal","31.79819","35.80986","background-location");
+    plotBackgroundLocation("Jerusalem","31.777444","35.234935","background-location");
+    plotBackgroundLocation("Hebron","31.524354","35.108539","background-location");
+    plotBackgroundLocation("Carmel","31.422876","35.132952","background-location");
+    plotBackgroundLocation("Gaza","31.503959","34.46203","background-location");
+    plotBackgroundLocation("Bethel","31.93053921","35.22103275","background-location");
+    }
+    
